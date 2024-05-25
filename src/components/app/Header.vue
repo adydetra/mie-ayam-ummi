3fef3aed1a357577f1373f9175c1d96efe716ea6
devdewa <uchiasasuke123.sp@gmail.com>
2024-05-25 18:20:32 +0700

    refactor: applying attributify unocss

--- src/components/app/Header.vue
+++ src/components/app/Header.vue
@@ -7,9 +7,9 @@ const nav = [
 </script>
 
 <template>
-  <header class="mx-auto flexbetween py-6">
+  <header mx-auto flexbetween py-6>
     <NuxtLink to="/" text-xl font-bold>
-      <NuxtImg src="/logo.png" alt="" class="size-14" />
+      <NuxtImg src="/logo.png" alt="" size-14 />
     </NuxtLink>
     <div class="hidden items-center gap-8 text-sm md:flex">
       <NuxtLink
@@ -18,12 +18,12 @@ const nav = [
         :class="{
           'bg-gray-100 dark:bg-gray-900 px-3.5 py-1.5 rounded-full !opacity-100 transition-colors ease-in-out duration-300': route(navs.to),
         }"
-        class="link px-3.5 opacity-80 transition hover:opacity-100"
+        link px-3.5 opacity-80 transition hover:opacity-100
       >
         {{ navs.label }}
       </NuxtLink>
     </div>
-    <div class="flex items-center gap-2">
+    <div flex items-center gap-2>
       <AppSearch />
       <AppColorMode />
     </div>
